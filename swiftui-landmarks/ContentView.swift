2cfebfdde87470eb3011f4387776cb0e06199f92
Hiléo Andersson <hileomsi@gmail.com>
2019-11-18 17:56:41 -0300

    Initial Commit

---
+++ swiftui-landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-landmarks
+//
+//  Created by Hiléo Andersson on 18/11/19.
+//  Copyright © 2019 Hiléo Andersson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
